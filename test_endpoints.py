80f19ac1b8c512a167eb761d03749c7da7c71044
yu-xiu <xiuyutiffany@gmail.com>
2024-02-18 22:53:09 -0800

    update more testing in endpoints.py

--- test_endpoints.py
+++ test_endpoints.py
@@ -1,9 +1,7 @@
-import pytest
-import requests
 from flask import jsonify
 
 
-# test Flask PyMongo CRUD application
+# test Flask PyMongo CRUD application with mongomock
 
 
 def test_create_user(client):
@@ -18,10 +16,14 @@ def test_create_user(client):
     assert response.get_json() == {'Congrats!': 'User created successfully'}
 
 
-def test_time(client):
-    response = client.get(f'/time')
+def test_homepage(client):
+    response = client.get('/')
     assert response.status_code == 200
-    # Use mongomock for testing
+
+
+def test_time(client):
+    response = client.get('/time')
+    assert response.status_code == 200
 
 
 def test_create_existing_user(client):
@@ -41,98 +43,46 @@ def test_create_existing_user(client):
     assert response.get_json() == expected_data
 
 
-# BASE_URL = 'http://localhost:3000'
+def test_list_all_users(client):
+    user_data = {
+        'name': 'test',
+        'email': 'test@example.com',
+    }
+    response = client.post('/users/create', json=user_data)
+    assert response.status_code == 200
+    print(response.json)
+    assert response.get_json() == {'Congrats!': 'User created successfully'}
 
-# new_user_data = {
-#     'name': 'Liz K',
-#     'email': 'l.w.com'
-# }
-
-# existing_user_data = {
-#     'name': 'Jason Lee',
-#     'email': 'Jason.Lee.Wang@sample.com'
-# }
-
-# expected_all_users = {
-#     "Jason Lee",
-#     "Liz K"
-# }
-# user_name_to_delete = 'Liz K'
-# user_name_to_delete_none_existing = 'Liz K'
-# user_name_to_get_email = 'Jason Lee'
-# expected_email = 'Jason.Lee.Wang@sample.com'
+    response = client.get('/users')
+    assert response.status_code == 200
+    expected_all_users = {
+        'test'
+    }
+    expected_data = {'users': list(expected_all_users)}
+    assert response.get_json() == expected_data
 
 
-# def test_hi():
-#     assert '3' == '3'
+def test_list_user_email(client):
+    user_data = {
+        'name': 'test',
+        'email': 'test@example.com',
+    }
+    response = client.post('/users/create', json=user_data)
+    assert response.status_code == 200
+    print(response.json)
+    assert response.get_json() == {'Congrats!': 'User created successfully'}
+
+    response = client.get('/users/email')
+    assert response.status_code == 200
+    expected_data = {'error': 'please provide a valid user name'}
+    assert response.get_json() == expected_data
 
 
-# def test_time(client):
-#     response = client.get(f'/time')
-#     assert response.status_code == 200
-
-
-# def test_create_user(client):
-#     response = requests.post(f'{BASE_URL}/users/create', json=new_user_data)
-#     # using assert to check if it passed the test, and whether the acutal result mathchs the expected results
-#     assert response.status_code == 200
-#     expected_data = {
-#         'Congrats!': 'User created successfully'}
-#     assert response.json() == expected_data
-
-
-# def test_create_existing_user(client):
-#     response = requests.post(
-#         f'{BASE_URL}/users/create', json=existing_user_data)
-#     assert response.status_code == 200
-#     expected_data = {
-#         'warning': 'The given name is in use, please add another user'}
-#     assert response.json() == expected_data
-
-
-# def test_list_all_users():
-#     response = requests.get(
-#         f'{BASE_URL}/users'
-#     )
-#     assert response.status_code == 200
-#     expected_data = {
-#         'users': expected_all_users
-#     }
-#     actual_users = set(response.json()['users'])
-#     expected_users = expected_data['users']
-
-#     extra_users = expected_users - actual_users
-#     if extra_users:
-#         print("Extra users in the expected users:", extra_users)
-#     assert actual_users == expected_users
-
-
-# def test_list_user_email():
-#     response = requests.get(
-#         f'{BASE_URL}/users/email?name={user_name_to_get_email}'
-#     )
-#     assert response.status_code == 200
-#     expected_data = {
-#         user_name_to_get_email: expected_email
-#     }
-#     assert response.json() == expected_data
-
-
-# def test_delete_given_user():
-#     response = requests.delete(
-#         f'{BASE_URL}/users/delete?name={user_name_to_delete}')
-#     assert response.status_code == 200
-#     expected_data = {
-#         'Great!': 'successfully deleted the given user'
-#     }
-#     assert response.json() == expected_data
-
-
-# def test_delete_none_existing_user():
-#     response = requests.delete(
-#         f'{BASE_URL}/users/delete?name={user_name_to_delete_none_existing}')
-#     assert response.status_code == 200
-#     expected_data = {
-#         'error': 'no such user'
-#     }
-#     assert response.json() == expected_data
+def test_delete_null_user(client):
+    response = client.delete(
+        '/users/delete')
+    assert response.status_code == 200
+    expected_data = {
+        'error': 'must provide a user name'
+    }
+    assert response.get_json() == expected_data
